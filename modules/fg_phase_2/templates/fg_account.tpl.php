61ea915da84e5f177c220116cc608fd785be3b04
mrhanlon <mrhanlon@4aa319da-7f9a-48e4-9c47-15b002cb106e>
2011-08-19 16:47:56 +0000

    theming

--- modules/fg_phase_2/templates/fg_account.tpl.php
+++ modules/fg_phase_2/templates/fg_account.tpl.php
@@ -1,57 +1,67 @@
-<h2><?php print $profile->title; ?></h2>
 <div class="account-block profile-block">
-	<?php print $profile->content; ?>
+	<h2><?php print $profile->title; ?></h2>
+	<div class="block-content"><?php print $profile->content; ?></div>
 </div>
+<a href="#">Back to top</a>
 
-<h2><?php print $account_status->title; ?></h2>
 <div class="account-block hpc-block">
-	<?php print $account_status->content; ?>
+	<h2><?php print $account_status->title; ?></h2>
+	<div class="block-content"><?php print $account_status->content; ?></div>
 </div>
+<a href="#">Back to top</a>
 
 <?php if ($links) : ?>
-	<h2><?php print $links->title; ?></h2>
 	<div class="account-block links-block">
-		<?php print $links->content; ?>
+		<h2><?php print $links->title; ?></h2>
+		<div class="block-content"><?php print $links->content; ?></div>
 	</div>
+	<a href="#">Back to top</a>
 <?php endif; ?>
 
-<h2>Project summary</h2>
 <div class="account-block project-block">
-	<h3><?php print $my_projects->title; ?></h3>
-	<div>
-		<?php print $my_projects->content; ?>
-	</div>
-	
-	<h3><?php print $managed_projects->title; ?></h3>
-	<div>
-		<?php print $managed_projects->content; ?>
-	</div>
-	
-	<h3><?php print $member_projects->title; ?></h3>
-	<div>
-		<?php print $member_projects->content; ?>
-	</div>
-	
-	<h3><?php print $expert_projects->title; ?></h3>
-	<div>
-		<?php print $expert_projects->content; ?>
+	<h2>Project summary</h2>
+	<div class="block-content">
+		<h3><?php print $my_projects->title; ?></h3>
+		<div>
+			<?php print $my_projects->content; ?>
+		</div>
+		
+		<h3><?php print $managed_projects->title; ?></h3>
+		<div>
+			<?php print $managed_projects->content; ?>
+		</div>
+		
+		<h3><?php print $member_projects->title; ?></h3>
+		<div>
+			<?php print $member_projects->content; ?>
+		</div>
+		
+		<h3><?php print $expert_projects->title; ?></h3>
+		<div>
+			<?php print $expert_projects->content; ?>
+		</div>
 	</div>
 </div>
+<a href="#">Back to top</a>
 
-<h2>Content summary</h2>
 <div class="account-block contents-block">
-	<h3><?php print $manual_pages->title; ?></h3>
-	<div>
-		<?php print $manual_pages->content; ?>
-	</div>
-	
-	<h3><?php print $community_pages->title; ?></h3>
-	<div>
-		<?php print $community_pages->content; ?>
+	<h2>Content summary</h2>
+	<div class="block-content">
+		<h3><?php print $manual_pages->title; ?></h3>
+		<div>
+			<?php print $manual_pages->content; ?>
+		</div>
+		
+		<h3><?php print $community_pages->title; ?></h3>
+		<div>
+			<?php print $community_pages->content; ?>
+		</div>
 	</div>
 </div>
+<a href="#">Back to top</a>
 
-<h2><?php print $publications->title; ?></h2>
 <div class="account-block pubs-block">
-	<?php print $publications->content; ?>
+	<h2><?php print $publications->title; ?></h2>
+	<div class="block-content"><?php print $publications->content; ?></div>
 </div>
+<a href="#">Back to top</a>
