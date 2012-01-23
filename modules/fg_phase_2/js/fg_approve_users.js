62c99c7d396b0525bf0f1a6cefdce753df07df60
mrhanlon <mrhanlon@4aa319da-7f9a-48e4-9c47-15b002cb106e>
2012-01-23 19:42:29 +0000

    add projects to the approve users page

--- modules/fg_phase_2/js/fg_approve_users.js
+++ modules/fg_phase_2/js/fg_approve_users.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.fg_approve_users = function(context) {
 	$('.fg-approve-users-table').dataTable({
-		"aaSorting":[[7,'asc']],
+		"aaSorting":[[8,'asc']],
 		"bInfo": false,
 		"bPaginate": false,
 		"sDom": 'f<"clear">t<"clear">'
