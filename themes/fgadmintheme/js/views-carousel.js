e4a8737fb279debfc766ca63ed2ff19b62f948fe
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2012-03-07 16:17:04 -0600

    more updates

--- themes/fgadmintheme/js/views-carousel.js
+++ themes/fgadmintheme/js/views-carousel.js
@@ -1,10 +1,12 @@
 (function($) {
 	$(document).ready(function() {
-		var $nav = $('<div class="carousel-nav">');
-		$('.view-front-page-carousel').before($nav);
-		$('.view-front-page-carousel')
-			.scrollable({circular:true, items:'.view-content',easing:'easeOutExpo'})
-			.autoscroll({interval:5000})
-			.navigator({navi:'.carousel-nav'});
+		if ($('.view-front-page-carousel .views-row').length > 1) {
+			var $nav = $('<div class="carousel-nav">');
+			$('.view-front-page-carousel').before($nav);
+			$('.view-front-page-carousel')
+				.scrollable({circular:true, items:'.view-content',easing:'easeOutExpo'})
+				.autoscroll({interval:5000})
+				.navigator({navi:'.carousel-nav'});
+		}
 	});
 })(jQuery);
