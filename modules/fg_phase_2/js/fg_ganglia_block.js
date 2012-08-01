e90d3e2156bb99292e348b7e0883352c6c3151bd
happycodemonkey <cmarnold2@gmail.com>
2012-08-01 15:12:20 -0500

    Added monitor xml feed

--- modules/fg_phase_2/js/fg_ganglia_block.js
+++ modules/fg_phase_2/js/fg_ganglia_block.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.fg_phase_2_ganglia_block_form = function(context) {
 
-	if ($('#edit-cluster').val() != "") { 
+	if (!$('#edit-cluster').val()) { 
 		$('#edit-metric-wrapper').hide();
 		$('#edit-node-wrapper').hide();
 		$('#edit-report-type-wrapper').hide();
