ca4a6c36ea4b787d46e74ef83ed7b7ed49702221
happycodemonkey <cmarnold2@gmail.com>
2012-08-20 11:58:37 -0500

    Trying to fix js selectors so there can be 2 ganglia blocks

--- modules/fg_phase_2/js/fg_ganglia_block.js
+++ modules/fg_phase_2/js/fg_ganglia_block.js
@@ -1,5 +1,6 @@
 Drupal.behaviors.fg_phase_2_ganglia_block_form = function(context) {
 
+	/**
 	if ($('.cluster').val() == "") { 
 		$('.node').hide();
 		if ($('.report-type').val() != "") {
@@ -14,22 +15,23 @@ Drupal.behaviors.fg_phase_2_ganglia_block_form = function(context) {
 		}
 		$('.period').hide();
 	}
+	**/
 
 	$('.metric').bind('change', function() {
 		if ($(this).val()) {
-			$(this).next('.report_type').hide();
+			$(this).find('.report_type').hide();
 		} else {
-			$(this).next('.report_type').show();
+			$(this).find('.report_type').show();
 		}
 	});
 
 	$('.report_type').bind('change', function() {
 		if ($(this).val()) {
-			$(this).next('.metric').hide();
-			$(this).next('.node').hide();
+			$(this).find('.metric').hide();
+			$(this).find('.node').hide();
 		} else {
-			$(this).next('.metric').show();
-			$(this).next('.node').show();
+			$(this).find('.metric').show();
+			$(this).find('.node').show();
 		}
 
 	});
@@ -43,21 +45,21 @@ Drupal.behaviors.fg_phase_2_ganglia_block_form = function(context) {
 				success: function (resp) {
 					var options = Drupal.parseJson(resp);
 
-					$(this).next('.node option').remove();
-					$(this).next('.node').append("<option value = ''>Show option</option>");
+					$(this).find('.node option').remove();
+					$(this).find('.node').append("<option value = ''>Show option</option>");
 
 					$.each(options, function(key, value) {
-						$(this).next('.node').append("<option value = '" + key + "'>" + value + "</option>");
+						$(this).find('.node').append("<option value = '" + key + "'>" + value + "</option>");
 					});
 
-					$(this).next('.node').show();
+					$(this).find('.node').show();
 				}
 			});
 		} else {
-			$(this).next('.metric').hide();
-			$(this).next('.node').hide();
-			$(this).next('.report-type').hide();
-			$(this).next('.period').hide();
+			$(this).find('.metric').hide();
+			$(this).find('.node').hide();
+			$(this).find('.report-type').hide();
+			$(this).find('.period').hide();
 		}
 	});
 
@@ -65,27 +67,27 @@ Drupal.behaviors.fg_phase_2_ganglia_block_form = function(context) {
 		if ($(this).val()) {
 			$.ajax({
 				type: "POST",
-				url: "ajax-callback/" + $(this).next('.cluster').val() + "/" + $(this + ' option:selected').text(),
+				url: "ajax-callback/" + $(this).find('.cluster').val() + "/" + $(this + ' option:selected').text(),
 				success: function (resp) {
 					var options = Drupal.parseJson(resp);
 
-					$(this).next('.metric option').remove();
-					$(this).next('.metric').append("<option value = ''>Show option</option>");
+					$(this).find('.metric option').remove();
+					$(this).find('.metric').append("<option value = ''>Show option</option>");
 
 					$.each(options, function(key, value) {
 						//console.log(key + ": " + value);
-						$(this).next('.metric').append("<option value = '" + key + "'>" + value + "</option>");
+						$(this).find('.metric').append("<option value = '" + key + "'>" + value + "</option>");
 					});
 
-					$(this).next('.metric').show();
-					$(this).next('.report-type').show();
-					$(this).next('.period').show();
+					$(this).find('.metric').show();
+					$(this).find('.report-type').show();
+					$(this).find('.period').show();
 				}
 			});
 		} else {
-			$(this).next('.metric').hide();
-			$(this).next('.report-type').hide();
-			$(this).next('.period').hide();
+			$(this).find('.metric').hide();
+			$(this).find('.report-type').hide();
+			$(this).find('.period').hide();
 		}
 	});
 }
