80e714645dce779305c655bee4872abab1abd96f
happycodemonkey <cmarnold2@gmail.com>
2012-11-26 16:22:16 -0600

    Debugging

--- misc/fg_multi_media_taxonomy.php
+++ misc/fg_multi_media_taxonomy.php
@@ -6,6 +6,7 @@
  *	Rewritten from fg_projects_taxonomy. Just trying to save myself time. 
 **/
 $multi_media_query = db_query("SELECT n.nid from {content_type_multi_media} ctmm left join {node} n on (ctmm.nid = n.nid)");
+$term = taxonomy_get_term_by_name("Science Cloud Summer School");
 
 // we pull all the keywords already in the database...it's a comma-deliniated list, so we need to explode and loop over the values
 // we also "clean" the keyword by lower-casing and trimming it so they're uniform
@@ -16,7 +17,6 @@ while ($result = db_fetch_object($multi_media_query)) {
 
 	if (empty($multi_media_node->taxonomy)) {
 		print "No term applied to node.\n";
-		$term = taxonomy_get_term_by_name("Science Cloud Summer School");
 		$multi_media_node->taxonomy[] = $term;
 			//taxonomy_get_term_by_name("Science Cloud Summer School");
 		//$term = taxonomy_get_term_by_name("Science Cloud Summer School");
