87057cf843f5dc41a0389382dd8ad6b28aad14fb
happycodemonkey <cmarnold2@gmail.com>
2013-01-23 16:03:40 -0600

    Fixed my previous commit :)

--- themes/fgtheme/templates/page-front.tpl.php
+++ themes/fgtheme/templates/page-front.tpl.php
@@ -129,7 +129,7 @@
       <?php if ($logo): ?>
 	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
 	<br />
-	<a id="xsede-link" href="https://xsede.org">an XSEDE resource provider</a>
+<!--	<a id="xsede-link" href="https://xsede.org">an XSEDE resource provider</a> -->
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
