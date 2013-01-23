2a8cd35db64535c0e053b15c8ec5a334daa73196
happycodemonkey <cmarnold2@gmail.com>
2013-01-23 16:01:36 -0600

    Added notes field to user approval page with edit link

--- themes/fgtheme/templates/page-front.tpl.php
+++ themes/fgtheme/templates/page-front.tpl.php
@@ -127,7 +127,9 @@
     <div id="header"><div class="section clearfix">
 
       <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+	<br />
+	<a id="xsede-link" href="https://xsede.org">an XSEDE resource provider</a>
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
