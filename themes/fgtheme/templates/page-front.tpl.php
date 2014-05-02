5d57ce79aada4e65575182b6dff63203f5782615
Carrie <cmarnold@tacc.utexas.edu>
2014-05-02 16:10:32 -0500

    Finally found the stupid thing pushing down the menu

--- themes/fgtheme/templates/page-front.tpl.php
+++ themes/fgtheme/templates/page-front.tpl.php
@@ -137,7 +137,6 @@
 
       <?php if ($logo): ?>
 	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-	<br />
 <!--	<a id="xsede-link" href="https://xsede.org">an XSEDE resource provider</a> -->
       <?php endif; ?>
 
